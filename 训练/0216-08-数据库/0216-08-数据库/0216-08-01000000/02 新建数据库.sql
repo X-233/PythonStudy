7e0579a19dbf45f8c5ab63f586fd8df24ba6ed03
zhang <1972125164@qq.com>
2023-03-13 21:07:18 +0800

    修改文件

--- 训练/0216-08-数据库/0216-08-数据库/0216-08-01000000/02 新建数据库.sql
+++ 训练/0216-08-数据库/0216-08-数据库/0216-08-01000000/02 新建数据库.sql
@@ -4,3 +4,29 @@
     3. 将处理后的 changsha_result.csv 数据插入到数据库
 */
 
+show databases;
+
+use python;
+
+show tables;
+
+select * from bads;
+
+drop table bads;
+
+create table bads(
+                     city varchar(255),          # 城市
+                     region varchar(255),        # 行政区
+                     title varchar(255),         # 门店名称
+                     star_level varchar(255),    # 星级
+                     star float,          # 星级得分
+                     review_num float,    # 点评总数
+                     mean_price varchar(20),    # 人均消费
+                     comment_list1 varchar(255), # 口味
+                     comment_list2 varchar(255), # 环境
+                     comment_list3 varchar(255), # 环境
+                     link varchar(255),          # 链接网址
+                     shop_tag_cate_click varchar(255),
+                     shop_tag_region_click varchar(255),
+                     addr varchar(255)
+);
