877b791840288712528204fcb5f281a1e5c099ed
zhang <1972125164@qq.com>
2023-03-13 11:55:18 +0800

    修改文件

---
+++ 学习/配置文件/数据库.py
@@ -0,0 +1,13 @@
+# 1、爬虫速度不要太快，不要给对方服务器造成太大压力
+# 2、爬虫不要伪造VIP，绕过对方身份验证，可以买一个VIP做自动化
+# 3、公民个人信息不要去碰
+
+
+redis_link = [
+    # 张辉
+    {'redis_key': 'zhanglin', 'redis_port': 6379, 'ip': '180.76.187.206', 'redis_name': 'default'},
+]
+mysql_link = [
+    {'mysql_name': 'zhanglin', 'mysql_key': 'zhanglin', 'mysql_ip': '180.76.187.206', 'mysql_port': 3306, 'mysql_database': '毕设'},
+    # 毕设
+  ]
